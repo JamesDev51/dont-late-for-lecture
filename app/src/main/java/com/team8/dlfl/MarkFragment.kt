21abf3a15e51a3884e964344f6888507df64d8dc
begojara99 <pch990824@naver.com>
2022-11-10 19:14:16 +0900

    네비게이션 완료

---
+++ app/src/main/java/com/team8/dlfl/MarkFragment.kt
@@ -0,0 +1,34 @@
+package com.team8.dlfl
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.team8.dlfl.databinding.FragmentMarkBinding
+
+
+class MarkFragment : Fragment() {
+
+    var binding: FragmentMarkBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMarkBinding.inflate(inflater)
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.btnPlus?.setOnClickListener {  //  즐겨찾기 등록 페이지 가기
+            findNavController().navigate(R.id.action_markFragment_to_addFragment)
+        }
+        //  실시간 지하철 위치정보 페이지는 리사이클러 뷰를 이용해서 가기
+    }
+
+
+}
