9b4920f7a555041b06e981077f012d7de457c781
begojara99 <pch990824@naver.com>
2022-11-23 14:17:26 +0900

    최신알림 HttpURLConnection

---
+++ app/src/main/java/com/team8/dlfl/adapter/TwitterAdapter.kt
@@ -0,0 +1,39 @@
+package com.team8.dlfl.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.team8.dlfl.R
+import com.team8.dlfl.databinding.ItemNoticeBinding
+import com.team8.dlfl.viewmodel.Notice
+
+class TwitterAdapter(val notices: LiveData<ArrayList<Notice>>): RecyclerView.Adapter<TwitterAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemNoticeBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(notice: Notice?) {
+            notice?.let {
+                binding.iconPeople.setImageResource(when(notice.people) {
+                    "true" -> R.drawable.solidarity
+                    "false" -> R.drawable.corporation
+                    else -> {
+                        R.drawable.twitter
+                    }
+                })
+                binding.txtTitle.text = it.title
+                binding.txtName.text = it.name
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemNoticeBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(notices.value?.getOrNull(position))
+    }
+
+    override fun getItemCount(): Int = notices.value?.size?: 0
+}
---
+++ app/src/main/java/com/team8/dlfl/fragment/Twitter.kt
@@ -0,0 +1,10 @@
+//package com.team8.dlfl.fragment
+//
+//enum class EPeople {
+//    SOLIDARITY,
+//    CORPORATION
+//}
+//data class TwitterModel(val source: String, val writer: String, val date: String, val contents: String) {
+//    lateinit var uid:String
+//    constructor(): this("","","","")
+//}
