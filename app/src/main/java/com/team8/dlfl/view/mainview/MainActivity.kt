3a0a8466b9e20e6e30766253b577f9d6a48df02d
begojara99 <pch990824@naver.com>
2022-11-30 13:32:01 +0900

    FCM 기능 테스트 성공

--- app/src/main/java/com/team8/dlfl/view/mainview/MainActivity.kt
+++ app/src/main/java/com/team8/dlfl/view/mainview/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.team8.dlfl.view.mainview
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.team8.dlfl.databinding.ActivityMainBinding
 import com.team8.dlfl.R
+import com.team8.dlfl.databinding.ActivityMainBinding
+import com.team8.dlfl.service.MyFirebaseMessagingService
 import com.team8.dlfl.view.authview.AuthActivity
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +33,28 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNav.setupWithNavController(navController)
         setContentView(binding.root)
+
+        // FCM 설정, Token 값 가져오기
+        MyFirebaseMessagingService().getFirebaseToken()
+
+
+        // DynamicLink 수신확인
+        initDynamicLink()
+    }
+
+    // DynamicLink 동적 링크
+    private fun initDynamicLink() {
+        val dynamicLinkData = intent.extras
+        if (dynamicLinkData != null) {
+            var dataStr = "DynamicLink 수신받은 값\n"
+            for (key in dynamicLinkData.keySet()) {
+                dataStr += "key: $key / value: ${dynamicLinkData.getString(key)}\n"
+            }
+
+            //binding.tvToken.text = dataStr // 테스트용으로 화면에 보여주려고 넣은듯
+            // 이걸로 즐찾에 있는 목록 표시할수 있을 듯
+        }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
